34102e84eaddbfcd4cd8abea61750c0eafbeb280
kkobziev <kostya.kobzev@gmail.com>
2020-01-04 00:12:32 +0200

    Add in-memory DB for storing exchange rates.

---
+++ CurrencyExchangeService/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD','UAH',23,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EUR','UAH',25,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'BTC','UAH',150000,0);
