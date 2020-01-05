6ade39dfdf295aa4f893d1cb6ddd1b10b87fd842
kkobziev <kostya.kobzev@gmail.com>
2020-01-05 18:49:37 +0200

    Add ribbon load balancing.

--- CurrencyCalculationService/src/main/java/org/example/microservices/CurrencyExchangeServiceProxy.java
+++ CurrencyCalculationService/src/main/java/org/example/microservices/CurrencyExchangeServiceProxy.java
@@ -1,12 +1,14 @@
 package org.example.microservices;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "127.0.0.1:8000")
+//@FeignClient(name = "currency-exchange-service", url = "127.0.0.1:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
-
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyCalculationResponse retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 }
