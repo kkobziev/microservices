ebf29e084b508c6b3a39155b3c7ade94e91c7f42
kkobziev <kostya.kobzev@gmail.com>
2020-01-05 19:16:14 +0200

    Connect all services to Eureka naming server.

--- CurrencyCalculationService/src/main/java/org/example/microservices/CurrencyCalculationService.java
+++ CurrencyCalculationService/src/main/java/org/example/microservices/CurrencyCalculationService.java
@@ -2,10 +2,12 @@ package org.example.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("org.example.microservices")
+@EnableDiscoveryClient
 public class CurrencyCalculationService {
     public static void main(String[] args) {
         SpringApplication.run(CurrencyCalculationService.class, args);
